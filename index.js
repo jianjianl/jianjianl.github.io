997731bcdf2adde719a5e90cfe48568f5cbad252
刘健岚 <liujianlan@bytedance.com>
2018-02-02 20:07:28 +0800

    [build]

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import App from 'component/app.vue';
-import router from 'router.js';
+define('index', function(require, exports, module) {
 
-Vue.use(VueRouter);
+  'use strict';
+  
+  function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+  
+  var _vue = require('node_modules/vue/dist/vue.runtime.common');
+  
+  var _vue2 = _interopRequireDefault(_vue);
+  
+  var _vueRouter = require('node_modules/vue-router/dist/vue-router.common');
+  
+  var _vueRouter2 = _interopRequireDefault(_vueRouter);
+  
+  var _componentAppVue = require('component/app.vue');
+  
+  var _componentAppVue2 = _interopRequireDefault(_componentAppVue);
+  
+  var _routerJs = require('router');
+  
+  var _routerJs2 = _interopRequireDefault(_routerJs);
+  
+  _vue2['default'].use(_vueRouter2['default']);
+  
+  new _vue2['default']({
+      el: "#blog",
+      router: _routerJs2['default'],
+      render: function render(h) {
+          return h(_componentAppVue2['default']);
+      }
+  });
 
-new Vue({
-    el: "#blog",
-    router,
-    render: h => h(App)
 });
--- router.js
+++ router.js
@@ -1,23 +1,39 @@
-import VueRouter from 'vue-router';
-import remark from 'component/remark';
-import tag from 'component/tag';
+define('router', function(require, exports, module) {
 
-const routes = [
-    {   
-        path: '/main/:id', 
-        component: remark,
-        props: true
-    },
-    {path: '/tag', component: tag}
-];
+  'use strict';
+  
+  Object.defineProperty(exports, '__esModule', {
+      value: true
+  });
+  
+  function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+  
+  var _vueRouter = require('node_modules/vue-router/dist/vue-router.common');
+  
+  var _vueRouter2 = _interopRequireDefault(_vueRouter);
+  
+  var _componentRemark = require('component/remark.vue');
+  
+  var _componentRemark2 = _interopRequireDefault(_componentRemark);
+  
+  var _componentTag = require('component/tag.vue');
+  
+  var _componentTag2 = _interopRequireDefault(_componentTag);
+  
+  var routes = [{
+      path: '/main/:id',
+      component: _componentRemark2['default'],
+      props: true
+  }, { path: '/tag', component: _componentTag2['default'] }];
+  
+  var router = new _vueRouter2['default']({
+      mode: 'history',
+      routes: routes
+  });
+  
+  router.beforeEach(function (to, from, next) {});
+  
+  exports['default'] = router;
+  module.exports = exports['default'];
 
-const router = new VueRouter({
-    mode: 'history',
-    routes
 });
-
-router.beforeEach((to, from, next) => {
-    
-})
-
-export default router;
